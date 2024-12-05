2ca30aaa8e7beba1d93bf0bf38724352b80f0391
Oli Radlett <o.radlett@gmail.com>
2024-12-05 13:07:45 +0000

    Add files via upload

---
+++ IRegistryValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_Mentoring_Project
+{
+    internal interface IRegistryValues
+    {
+        string DeviceTypeID{ get; }
+        string DeviceInstanceID { get; }
+        string HardwareID { get; }
+        public void Print();
+    }
+}
---
+++ RegistryValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_Mentoring_Project
+{
+    internal class RegistryValues: IRegistryValues
+    {
+        public RegistryValues(string deviceTypeId, string deviceInstanceId, string hardwareId)
+        {
+            DeviceTypeID = deviceTypeId;
+            DeviceInstanceID = deviceInstanceId;
+            HardwareID = hardwareId;
+        }
+
+        public string DeviceTypeID{ get; }
+        public string DeviceInstanceID{ get; }
+        public string HardwareID { get; }
+
+        public void Print()
+        {
+            Console.WriteLine("-------------------------------------------------------------------------------------");
+            Console.WriteLine($"DeviceTypeID: {DeviceTypeID}");
+            Console.WriteLine($"DeviceInstanceID: {DeviceInstanceID}");
+            Console.WriteLine($"HardwareID: {HardwareID}");
+        }
+
+    }
+}
